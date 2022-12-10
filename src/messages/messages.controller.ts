56979b95c7895ec831264bfd2f6ab0f2493f3e53
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-12-10 13:01:28 +0700

    #3 Inject the MessagesService into the MessagesController

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,29 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  // @Get()
-  // getAllMessages() {}
+  messagesService: MessagesService;
+
+  constructor() {
+    // DON'T DO THIS ON REAL APP
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  getAllMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
