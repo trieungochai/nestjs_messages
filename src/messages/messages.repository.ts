7e3ce796d63503bb5fe628b33d3db3110c88356d
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-12-10 12:12:20 +0700

    #3 Reading and Writing to a Storage file

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -7,7 +7,20 @@ export class MessagesRepository {
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
 
-  async create(message: string) {}
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
